4115b3e57a3c8d3dcea42b862e263a0adb3dbc21
Andreas <Andreas@11af3d7c-2093-d24b-b0ef-1deae2c45b7c>
2008-03-02 20:26:19 +0000

    SiteDelta 0.2
    git-svn-id: file:///E/svn/sitedelta/trunk@3 11af3d7c-2093-d24b-b0ef-1deae2c45b7c

--- unpacked/content/overlay.js
+++ unpacked/content/overlay.js
@@ -3,19 +3,19 @@ var sitedelta = {
     // initialization code
     this.prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
     this.initialized = true;
+    this.lastURL=""; this.changes=0; this.curchange=0;
     this.strings = document.getElementById("sitedelta-strings");
 
     var file = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile)
     file.append("sitedelta");
     if( !file.exists() || !file.isDirectory() ) {
-      file.create(Components.interfaces.nsIFile.DIRECTORY_TYPE, 0664);
+      file.create(Components.interfaces.nsIFile.DIRECTORY_TYPE, 0755);
     }
   },
   onStatusBarButton: function(e) {
-   var body=content.document.getElementsByTagName("body")[0];
-   if(!body) {alert("Inhalt nicht gefunden. \nVielleicht sind Frames im Spiel - verwenden Sie dann bitte Aktueller Frame=>Nur diesen Frame anzeigen und versuchen Sie es erneut."); return; }
    var url=content.window.location.href; 
    url=url.replace(/^.*:\/\//, "");
+   url=url.replace(/#.*$/, "");
    url=url.replace(/\./g,'-');
    url=url.replace(/\%[0-9A-Fa-f]{2}/g, "_");
    url=url.replace(/[^a-zA-Z0-9\.]/g, "_");
@@ -23,6 +23,13 @@ var sitedelta = {
    url=url.replace(/_$/, "");
    url=url.replace(/^_/, "");
    if(url=='') url='_default_';
+   if(url==this.lastURL && this.changes>0 && content.document.getElementById("sitedelta-change"+this.curchange)) {
+    this.curchange=(this.curchange+1)%this.changes;
+    content.window.location.hash="#sitedelta-change"+this.curchange;
+    return;
+   }
+   var body=content.document.getElementsByTagName("body")[0];
+   if(!body) {alert("No content found"); return; }
 
    var file = Components.classes["@mozilla.org/file/directory_service;1"].getService(Components.interfaces.nsIProperties).get("ProfD", Components.interfaces.nsIFile);
    file.append("sitedelta"); file.append(url+".dat");
@@ -32,8 +39,9 @@ var sitedelta = {
     var fstream = Components.classes["@mozilla.org/network/file-input-stream;1"].createInstance(Components.interfaces.nsIFileInputStream); var is = Components.classes["@mozilla.org/intl/converter-input-stream;1"].createInstance(Components.interfaces.nsIConverterInputStream);
     fstream.init(file, -1, 0, 0); is.init(fstream,"UTF-8",1024,0xFFFD); var str={}; var numChars = is.readString(4096,str); while (numChars > 0) {last += str.value; numChars = is.readString(4096,str); } is.close(); fstream.close();
    }
-
+   this.changes=0; this.curchange=0; this.lastURL=url;
    var text=this.walkTree(body);
+   if(this.changes>0) content.window.location.hash="#sitedelta-change0";
 
    var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].createInstance(Components.interfaces.nsIFileOutputStream);
    var os = Components.classes["@mozilla.org/intl/converter-output-stream;1"].createInstance(Components.interfaces.nsIConverterOutputStream);
@@ -54,9 +62,8 @@ var sitedelta = {
      ret+=text;
       if(last.indexOf(text)==-1 && text!="") {
        hil=document.createElement("span");
-       hil.style.display="inline";
-       hil.style.border="dotted red 1px;";
-       hil.style.background="#ff8";
+       hil.setAttribute("style","display: inline; border: dotted red 1px; background: #ff8; color: #000;");
+       hil.id="sitedelta-change" + (this.changes++);
        hil.appendChild(cur.cloneNode(true));
        cur.parentNode.replaceChild(hil,cur);
        cur=hil;

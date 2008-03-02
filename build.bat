d2bd7ae19b320b44c96068c088941dbbb45bbcea
Andreas <Andreas@11af3d7c-2093-d24b-b0ef-1deae2c45b7c>
2008-03-02 20:20:36 +0000

    build script ignores .svn
    git-svn-id: file:///E/svn/sitedelta/trunk@2 11af3d7c-2093-d24b-b0ef-1deae2c45b7c

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 cd unpacked
 del ..\chrome\sitedelta.jar
-"C:\Program Files\7-Zip\7z" a -tzip ..\chrome\sitedelta.jar content locale skin
+"C:\Program Files\7-Zip\7z" a -tzip -xr!*.svn ..\chrome\sitedelta.jar content locale skin
 cd ..
 del SiteDelta.xpi
-"C:\Program Files\7-Zip\7z" a -tzip SiteDelta.xpi a install.rdf chrome defaults components
+"C:\Program Files\7-Zip\7z" a -tzip -xr!*.svn SiteDelta.xpi install.rdf chrome defaults components
 cd unpacked
-"C:\Program Files\7-Zip\7z" a -tzip ..\SiteDelta.xpi a chrome.manifest
+"C:\Program Files\7-Zip\7z" a -tzip -xr!*.svn ..\SiteDelta.xpi chrome.manifest
 cd ..

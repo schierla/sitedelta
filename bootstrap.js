0a49f04a24bba304621103a13bb55bff4d1757c3
Andreas Schierl <schierla@gmx.de>
2017-09-24 16:07:40 +0200

    better build script, using chrome.runtime

--- bootstrap.js
+++ bootstrap.js
@@ -1,40 +1,40 @@
 "use strict";
 
 function setDefaultPref(prefName, prefValue) {
-  var branch = Services.prefs.getDefaultBranch(null);
-  switch (typeof prefValue) {
-    case "number": return branch.setIntPref(prefName, prefValue);
-    case "boolean": return branch.setBoolPref(prefName, prefValue);
-    case "string": return branch.setCharPref(prefName, prefValue);
-  }
+	var branch = Services.prefs.getDefaultBranch(null);
+	switch (typeof prefValue) {
+		case "number": return branch.setIntPref(prefName, prefValue);
+		case "boolean": return branch.setBoolPref(prefName, prefValue);
+		case "string": return branch.setCharPref(prefName, prefValue);
+	}
 }
 
 function startup({ webExtension }) {
-  Components.utils.import("chrome://sitedelta/content/SiteDeltaExport.jsm");
-  Components.utils.import("resource://gre/modules/Services.jsm");
+	Components.utils.import("chrome://sitedelta/content/SiteDeltaExport.jsm");
+	Components.utils.import("resource://gre/modules/Services.jsm");
 
-  Services.scriptloader.loadSubScript("chrome://sitedelta/content/defaultprefs.js", { pref: setDefaultPref });
+	Services.scriptloader.loadSubScript("chrome://sitedelta/content/defaultprefs.js", { pref: setDefaultPref });
 
-  // Start the embedded webextension.
-  webExtension.startup().then(api => {
-    const { browser } = api;
-    browser.runtime.onMessage.addListener((msg, sender, sendReply) => {
-      if (msg == "getVersion") {
-        sendReply(1);
-      } if (msg == "getSettings") {
-        SiteDeltaExport.getSettings(sendReply);
-      } else if (msg == "getPages") {
-        SiteDeltaExport.getPages(sendReply);
-      } else if (msg == "getPresets") {
-        SiteDeltaExport.getPresets(sendReply);
-      }
-    });
-  });
+	// Start the embedded webextension.
+	webExtension.startup().then(api => {
+		const { browser } = api;
+		browser.runtime.onMessage.addListener((msg, sender, sendReply) => {
+			if (msg == "getVersion") {
+				sendReply(1);
+			} if (msg == "getSettings") {
+				SiteDeltaExport.getSettings(sendReply);
+			} else if (msg == "getPages") {
+				SiteDeltaExport.getPages(sendReply);
+			} else if (msg == "getPresets") {
+				SiteDeltaExport.getPresets(sendReply);
+			}
+		});
+	});
 }
 
 function shutdown(data) {
-  Components.utils.unload("resource://gre/modules/Services.jsm");
-  Components.utils.unload("chrome://sitedelta/content/SiteDeltaExport.jsm");
+	Components.utils.unload("resource://gre/modules/Services.jsm");
+	Components.utils.unload("chrome://sitedelta/content/SiteDeltaExport.jsm");
 }
 
 function install(data) {
--- webextension/background.js
+++ webextension/background.js
@@ -1,13 +1,11 @@
-browser.runtime.onMessageExternal.addListener(function(message, sender, callback) {
+chrome.runtime.onMessageExternal.addListener(function(message, sender, callback) {
     if(sender.id == "sitedelta-watch@schierla.de" || 
         sender.id == "sitedelta-highlight@schierla.de" || 
         sender.id == "eb1debe5-b520-48b6-8242-6deed7ecd0eb" ||
         sender.id == "42f46382-33c5-4728-9bec-a9a2e0c6397a") {
-        browser.runtime.sendMessage(message).then(reply => {
+        chrome.runtime.sendMessage(message, function(reply) {
+            if(!reply) console.log(chrome.runtime.lastError);
             callback(reply);
-        }, error => {
-            console.warn("Error: " + error);
-            callback(null);
         });
     } else {
         console.warn("SiteDelta: Disallowed access from '" + sender.id + "' for '" + message + "'");

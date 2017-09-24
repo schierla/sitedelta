83617130f355b8eeb927b6223396e201616dd206
Andreas Schierl <schierla@gmx.de>
2017-09-24 15:25:33 +0200

    build into build folder

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
-del SiteDelta.xpi
-"C:\Program Files\7-Zip\7z" a -tzip -xr!*.svn SiteDelta.xpi install.rdf chrome.manifest bootstrap.js chrome webextension
-
+mkdir build
+del build\SiteDelta.xpi
+"C:\Program Files\7-Zip\7z" a -tzip -xr!*.svn build\SiteDelta.xpi install.rdf chrome.manifest bootstrap.js chrome webextension

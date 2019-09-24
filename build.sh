0f782a790a9cb645da2c66ff275abed93aef7eff
Andreas Schierl <sitedelta@schierla.de>
2019-09-24 10:37:51 +0200

    Switch from JavaScript to TypeScript

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ if [ "A$1" != "A" ]; then
   cp -rp $1/* build/unpacked/
   cp -rp common build/unpacked/
   cp -rp _locales build/unpacked/
+  rm -rf build/common/scripts
+  rm -rf build/scripts
+  cp -rp build/typescript/common/scripts build/unpacked/common/
+  cp -rp build/typescript/$1/scripts build/unpacked/
   mv build/unpacked/$2.manifest.json build/unpacked/manifest.json
   rm build/unpacked/*.manifest.json
   if [ "A$3" == "Apack" ]; then
